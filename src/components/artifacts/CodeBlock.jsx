d34d1f99f8346f38bf88474d9d68bc6b7f9e5333
michaelidesg <george.michaelides@gmail.com>
2025-08-12 21:08:09 +0100

    first

--- src/components/artifacts/CodeBlock.jsx
+++ src/components/artifacts/CodeBlock.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const CodeBlock = ({ language, content }) => {
   const handleCopy = () => {
